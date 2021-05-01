d3410931a575438ab357e94c18bb310fc75b78ef
godu <arthur.weber@viacesi.fr>
2021-05-01 19:24:31 +0200

    The default world

--- src/RayTracer/Data/World.hs
+++ src/RayTracer/Data/World.hs
@@ -1,12 +1,17 @@
 module RayTracer.Data.World
   ( World (objects, light),
     world,
+    defaultWorld,
   )
 where
 
-import RayTracer.Data.Light (Light)
-import RayTracer.Data.Sphere (Sphere)
-import Prelude (Eq, Maybe (Nothing), Show)
+import RayTracer.Data.Color (color)
+import RayTracer.Data.Light (Light, pointLight)
+import qualified RayTracer.Data.Material as M (color, diffuse, material, specular)
+import RayTracer.Data.Sphere (Sphere, material, sphere, transformation)
+import RayTracer.Data.Tuple (point)
+import RayTracer.Transformation (scaling)
+import Prelude (Double, Eq, Maybe (Nothing), Show, return)
 
 data World a = World
   { objects :: [Sphere a],
@@ -14,4 +19,23 @@ data World a = World
   }
   deriving (Show, Eq)
 
+world :: World a
 world = World [] Nothing
+
+defaultWorld :: World Double
+defaultWorld = World [s1, s2] (return l)
+  where
+    s1 =
+      sphere
+        { material =
+            M.material
+              { M.color = color 0.8 1.0 0.6,
+                M.diffuse = 0.7,
+                M.specular = 0.2
+              }
+        }
+    s2 =
+      sphere
+        { transformation = scaling 0.5 0.5 0.5
+        }
+    l = pointLight (point (-10) 10 (-10)) (color 1 1 1)
--- test/RayTracer/Data/WorldSpec.hs
+++ test/RayTracer/Data/WorldSpec.hs
@@ -3,17 +3,27 @@ module RayTracer.Data.WorldSpec
   )
 where
 
-import RayTracer.Data.World
-  ( World (light, objects),
-    world,
+import RayTracer.Data.Color (color)
+import RayTracer.Data.Light (pointLight)
+import qualified RayTracer.Data.Material as M
+  ( Material (ambient, specular),
+    color,
+    diffuse,
+    material,
   )
+import RayTracer.Data.Sphere (material, sphere, transformation)
+import RayTracer.Data.Tuple (point)
+import RayTracer.Data.World (World (light, objects), defaultWorld, world)
+import RayTracer.Transformation (scaling)
 import Test.Hspec
   ( Spec,
     it,
     shouldBe,
+    shouldContain,
   )
 import Prelude
   ( Maybe (Nothing),
+    return,
     ($),
   )
 
@@ -23,3 +33,23 @@ spec = do
     let actual = world
     objects actual `shouldBe` []
     light actual `shouldBe` Nothing
+
+  it "The default world" $ do
+    let l = pointLight (point (-10) 10 (-10)) (color 1 1 1)
+    let s1 =
+          sphere
+            { material =
+                M.material
+                  { M.color = color 0.8 1.0 0.6,
+                    M.diffuse = 0.7,
+                    M.specular = 0.2
+                  }
+            }
+    let s2 =
+          sphere
+            { transformation = scaling 0.5 0.5 0.5
+            }
+    let w = defaultWorld
+    light w `shouldBe` return l
+    objects w `shouldContain` [s1]
+    objects w `shouldContain` [s2]
